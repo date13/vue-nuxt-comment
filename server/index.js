2c3c666329f5d2e352c4107277a0e9859b561bcb
date13 <date13@hotmail.com>
2020-08-04 11:24:16 +0800

    Update index.js
    local https and prod http start app serve

--- server/index.js
+++ server/index.js
@@ -1,4 +1,5 @@
 const Koa = require('koa')
+// const consola = require('consola')
 const { Nuxt, Builder } = require('nuxt')
 const logger = require('koa-logger');
 const app = new Koa()
@@ -8,38 +9,47 @@ const fs = require('fs');
 // Import and Set Nuxt.js options
 const config = require('../nuxt.config.js')
 
-config.dev = app.env !== 'production'
+/*const type =
+  process.env.NODE_ENV_APP === 'stage' ? 'stage' : process.env.NODE_ENV
+process.env.innertApiHost = env[type].innertApiHost*/
+// config.dev = app.env === 'dev'
 async function start() {
   // Instantiate nuxt.js
   const nuxt = new Nuxt(config)
   const {
     host = process.env.HOST || '127.0.0.1',
-    port = process.env.PORT || 3000
+    port = process.env.PORT || 8080
   } = nuxt.options.server
 
-  // Build in development
-  if (config.dev) {
-    const builder = new Builder(nuxt)
-    await builder.build()
-  } else {
-    await nuxt.ready()
-  }
-  let options = {
-    key: fs.readFileSync('./keys/server.key', 'utf8'),  //ssl文件路径
-    cert: fs.readFileSync('./keys/server.cert', 'utf8')  //ssl文件路径
-  };
-  app.use(sslify())
-  app.use(logger())
+
   app.use((ctx) => {
     ctx.status = 200
     ctx.respond = false // Bypass Koa's built-in response handling
     ctx.req.ctx = ctx // This might be useful later on, e.g. in nuxtServerInit or with nuxt-stash
     nuxt.render(ctx.req, ctx.res)
   })
-  
-  https.createServer(options, app.callback()).listen(port, () => {
-    console.log(`server running success at ${port}`)
-  });
+  let options = {};
+  // Build in development
+  if (config.dev) {
+    const builder = new Builder(nuxt)
+    await builder.build()
+    options = {
+      key: fs.readFileSync('./keys/server.key', 'utf8'),  //ssl文件路径
+      cert: fs.readFileSync('./keys/server.cert', 'utf8')  //ssl文件路径
+    };
+    app.use(sslify())
+    app.use(logger())
+    https.createServer(options, app.callback()).listen(port, () => {
+      console.log(`server running success at ${port}`)
+    });
+  } else {
+    await nuxt.ready();
+    app.listen(port, host)
+    consola.ready({
+      message: `Server listening on http://${host}:${port}`,
+      badge: true
+    })
+  }
 }
 
 start()
